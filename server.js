d204b0037c237833189fcddda41dd90f99dc2cba
Max Stern <stern.maxwell@gmail.com>
2019-05-25 15:13:30 -0700

    user schema

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const app = express();
 //DATABASE
 connectDB();
 
+// MIDDLEWARE
+app.use(express.json({
+  extended: false
+}));
+
 app.get('/', (req, res) => res.send('API Running'));
 
 // ROUTES
