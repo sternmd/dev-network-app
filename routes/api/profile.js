8a58298bb61d44246a7b747bff8f33712ee6d422
Max Stern <stern.maxwell@gmail.com>
2019-05-27 19:38:13 -0700

    profile route, post

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const {
+  check,
+  validationResult
+} = require('express-validator/check');
 
-const Profile = require('../../models/Profile')
-const User = require('../../models/User')
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
 // @route  GET api/profile/me
 // @desc   Get current user's profile
@@ -29,6 +33,14 @@ router.get('/me', auth, async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+
+// @route  POST api/profile
+// @desc   Create or update a user profile
+// @access Private
+
+router.post('/', [auth, [check('status', 'Status is required').not().isEmpty()]], (req, res) => {
+
+})
 
 
 module.exports = router;
