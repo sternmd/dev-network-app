6676ccd6bae149bd8a38ce2c3c3a4a037934ef00
Max Stern <stern.maxwell@gmail.com>
2019-05-29 23:33:15 -0700

    post like route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -91,7 +91,6 @@ router.delete('/:id', auth, async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
 
-
     if (!post) {
       return res.status(404).json({
         msg: 'Post not found.'
@@ -118,6 +117,32 @@ router.delete('/:id', auth, async (req, res) => {
         msg: 'Post not found.'
       })
     }
+    res.status(500).send('Server error.')
+  }
+})
+
+// @route  PUT api/posts/like/:id
+// @desc   Like a post
+// @access Private
+router.put('/likes/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.param.id);
+
+    //Check if the post has already been liked
+    if (post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+      return res.status(400).json({
+        msg: 'Post already liked.'
+      })
+    }
+
+    post.likes.unshift({
+      user: req.user.id
+    });
+    await post.save();
+    res.json(post.likes);
+
+  } catch (error) {
+    console.error(err.message);
     res.status(500).send('Server error.')
   }
 })
